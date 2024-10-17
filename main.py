c0325eb4899defa4496176555b3d3ae089de16eb
Gurpinder60 <72954248+Gurpinder60@users.noreply.github.com>
2024-10-17 08:42:32 -0700

    main.py

--- main.py
+++ main.py
@@ -1,100 +1,104 @@
-import tkinter
-
-window = tkinter.Tk()
-
-window.title("My first program")
-window.minsize(width=550, height=400)
-
-my_label = tkinter.Label(text="Label", font=("Arial", 24, "bold"))
-#.pack() is used to print label on window
-my_label.pack()
-
-
-window.mainloop()
-
 from tkinter import *
+from tkinter import messagebox
+import pyperclip
+import json
 
-#Creating a new window and configurations
+from random import randint, choice, shuffle
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def generate_password():
+    password_letter = [choice(letters) for _ in range(randint(8, 10))]
+    symbols_letter = [choice(symbols) for _ in range(randint(2, 4))]
+    numbers_letter = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letter + symbols_letter + numbers_letter
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
+
+
+def save():
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+    new_data = {website:{
+        "email": email, "password": password,
+    }}
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="ooops", message="one of the field is empty")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+
+        finally:
+            website_input.delete(0, END)
+            password_input.delete(0, END)
+
+def search_website():
+    website = website_input.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="Data not found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title="oops", message=f"Email: {email}\n Password: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists")
+
+
 window = Tk()
-window.title("Widget Examples")
-window.minsize(width=500, height=500)
+window.title("Password Manager")
+window.config(padx=30, pady=30)
 
-#Labels
-label = Label(text="This is old text")
-label.config(text="This is new text")
-label.pack()
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=0)
 
-#Buttons
-def action():
-    print("Do something")
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
 
-#calls action() when pressed
-button = Button(text="Click Me", command=action)
-button.pack()
+website_input = Entry(width=40, justify='left')
+website_input.grid(row=1,column=1, columnspan=2)
+website_input.focus()
 
-#Entries
-entry = Entry(width=30)
-#Add some text to begin with
-entry.insert(END, string="Some text to begin with.")
-#Gets text in entry
-print(entry.get())
-entry.pack()
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
 
-#Text
-text = Text(height=5, width=30)
-#Puts cursor in textbox.
-text.focus()
-#Adds some text to begin with.
-text.insert(END, "Example of multi-line text entry.")
-#Get's current value in textbox at line 1, character 0
-print(text.get("1.0", END))
-text.pack()
+email_input = Entry(width=40)
+email_input.grid(column=1, row=2, columnspan=2)
+email_input.insert(0, "gurbrar@email.com")
 
-#Spinbox
-def spinbox_used():
-    #gets the current value in spinbox.
-    print(spinbox.get())
-spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
-spinbox.pack()
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
 
-#Scale
-#Called with current scale value.
-def scale_used(value):
-    print(value)
-scale = Scale(from_=0, to=100, command=scale_used)
-scale.pack()
+password_input = Entry(width=21)
+password_input.grid(column=1, row=3)
 
-#Checkbutton
-def checkbutton_used():
-    #Prints 1 if On button checked, otherwise 0.
-    print(checked_state.get())
-#variable to hold on to checked state, 0 is off, 1 is on.
-checked_state = IntVar()
-checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
-checked_state.get()
-checkbutton.pack()
+add_button = Button(text="Add", width=25, command=save)
+add_button.grid(column=1, row=4, columnspan=2)
 
-#Radiobutton
-def radio_used():
-    print(radio_state.get())
-#Variable to hold on to which radio button value is checked.
-radio_state = IntVar()
-radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
-radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
-radiobutton1.pack()
-radiobutton2.pack()
+generate_button = Button(text="Generate Password", command=generate_password)
+generate_button.grid(column=2, row=3)
 
+search_button = Button(text="Search Website",width=13, command=search_website)
+search_button.grid(row=1, column=2)
 
-#Listbox
-def listbox_used(event):
-    # Gets current selection from listbox
-    print(listbox.get(listbox.curselection()))
-
-listbox = Listbox(height=4)
-fruits = ["Apple", "Pear", "Orange", "Banana"]
-for item in fruits:
-    listbox.insert(fruits.index(item), item)
-listbox.bind("<<ListboxSelect>>", listbox_used)
-listbox.pack()
 window.mainloop()
-
